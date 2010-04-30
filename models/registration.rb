142ca76f0a54f464d38bd7d496e9114c4b77b828
Dana Merrick <dana@ics.com>
2010-04-29 23:02:41 -0400

    Making the code a bit more readable.

--- models/registration.rb
+++ models/registration.rb
@@ -3,9 +3,13 @@ class Registration
   
   property :id, Serial
 
-  property :content, String, :default => lambda { |r, p| 
-    (1..6).collect { (i = Kernel.rand(62); i += ((i < 10) ? 48 : ((i < 36) ? 55 : 61 ))).chr }.join.downcase }
-    
+  property :content, String, :default => lambda do |r, p| 
+    # generate a random 6 character alphanumeric string
+    (1..6).collect do
+      (i = Kernel.rand(62); i += ((i < 10) ? 48 : ((i < 36) ? 55 : 61 ))).chr
+    end.join.downcase 
+  end
+  
   property :ip, String
   
   property :used, Boolean, :default => false
